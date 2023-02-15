6fac99e98b91f63dcf22746f9cb5520b6ec9e12f
Jakub Wojtysiak <it.jakub.wojtysiak@gmail.com>
2023-02-15 16:52:43 +0100

    Text typho fix

--- product-list-frontend/src/components/Footer/Footer.component.jsx
+++ product-list-frontend/src/components/Footer/Footer.component.jsx
@@ -3,7 +3,7 @@ import "./Footer.scss"
 export default function Footer(){
     return(
         <footer className="footer">
-            <p>Scandiweb Test assigment</p>
+            <p>Scandiweb Test assignment</p>
         </footer>
     )
 }
