f8bbd54d00bb837b5a34b4cce75a84220e445b5a
Jakub Wojtysiak <it.jakub.wojtysiak@gmail.com>
2023-02-15 18:20:22 +0100

    Added fetch data from PHP and display to user functionality

--- product-list-frontend/src/components/Product List Section/ProductListSection.component.jsx
+++ product-list-frontend/src/components/Product List Section/ProductListSection.component.jsx
@@ -1,45 +1,48 @@
+import { useEffect, useState } from "react";
 import ProductListItem from "./Product List Item/ProductListItem.component";
 import "./ProductListSection.scss";
 
 export default function ProductListSection() {
-  return (
-    <section className="product-list-section">
-      <ProductListItem
-        sku="SKU200123"
-        name="Acame DISC"
-        price="1.00"
-        properties="700 MB"
-      />
-      <ProductListItem
-        sku="SKU200123"
-        name="Acame DISC"
-        price="1.00"
-        properties="700 MB"
-      />
-      <ProductListItem
-        sku="SKU200123"
-        name="Acame DISC"
-        price="1.00"
-        properties="700 MB"
-      />
-      <ProductListItem
-        sku="SKU200123"
-        name="Acame DISC"
-        price="1.00"
-        properties="700 MB"
-      />
-      <ProductListItem
-        sku="SKU200123"
-        name="Acame DISC"
-        price="1.00"
-        properties="700 MB"
-      />
-      <ProductListItem
-        sku="SKU200123"
-        name="Acame DISC"
-        price="1.00"
-        properties="700 MB"
-      />
-    </section>
-  );
+  const [productList, setProductLIst] = useState();
+
+  useEffect(() => {
+    fetchData().then((products) => {
+      setProductLIst(
+        products.map((product) => {
+          return (
+            <ProductListItem
+              key={product.id}
+              sku={product.sku}
+              name={product.name}
+              price={product.price}
+              type={product.type}
+              height={product.height}
+              length={product.length}
+              width={product.width}
+              weight={product.weight}
+              size={product.size}
+            />
+          );
+        })
+      );
+    });
+  }, []);
+
+  // Fetch data with all saved products from PHP server
+  async function fetchData() {
+    let products;
+    const response = await fetch(
+      "http://127.0.0.1/Product%20List%20(React%20+%20PHP)/product-list-backend/includes/get-product-list.php"
+    );
+
+    if (response.status === 200) {
+      products = await response.json();
+    } else {
+      console.log("DB Error");
+    }
+
+    return products;
+  }
+
+  return <section className="product-list-section">{productList}</section>;
 }
